dfb56e7ef9a14c5170b8e863ac7864e4f11c413a
xAdh2m <43392967+xAdh2m@users.noreply.github.com>
2018-09-19 02:34:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,22 +33,8 @@ client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(` 
 **
-:kissing::heart::eyes: حيااكْ الله  فى سيرفرنا 
-╔╦╦╦═╦╗╔═╦═╦══╦═╗
-║║║║╩╣╚╣═╣║║║║║╩╣
-╚══╩═╩═╩═╩═╩╩╩╩═╝ 
-
-‏نقدم لك دعوة لسموك الراقيَ .
-‏لنحظى بجمال حضورك وتواجدك
-•بتنـورونـا الجميع :wink:
-•اي مشكله توآجهك ولا يهمك. عندك الاداره تفضل ونحل مشكلتك مع آي شخص بالسيرفر.:telephone_receiver:
-•عندك فكره موضوع تفضل شآركنا فيه:jack_o_lantern::gift:
-•واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
-شرفنا يا عسل:wink: :kissing_heart:
-
-https://discord.gg/FQXf32Q
-joined info-selfie
-! Selfie.                              
+Welcome to NewEra
+https://discord.gg/Th8YJcu                          
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
 **`) 
     }
